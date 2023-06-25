4003736c24eda6d3fcca5184a64c4be4bc6d22a1
Michael Mathu <mikemathu.dev@gmail.com>
2023-06-25 15:25:58 +0300

    Add project files.

---
+++ Controllers/AccountsController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Accounts.Controllers
+{
+    public class AccountsController : Controller
+    {
+        public IActionResult FiscalPeriods()
+        {
+            return View();
+        }
+        public IActionResult GeneralLedgerAccounts()
+        {
+            return View();
+        } 
+        public IActionResult PaymentModes()
+        {
+            return View();
+        }
+        public IActionResult Banks()
+        {
+            return View();
+        } 
+        public IActionResult CashierShifts()
+        {
+            return View();
+        }
+        public IActionResult JournalVouchers()
+        {
+            return View();
+        } 
+        public IActionResult Taxes()
+        {
+            return View();
+        } 
+        public IActionResult CashTransfers()
+        {
+            return View();
+        }
+        public IActionResult BankDeposits()
+        {
+            return View();
+        }
+        public IActionResult Cheques()
+        {
+            return View();
+        }
+        public IActionResult BankReconciliation()
+        {
+            return View();
+        } 
+        public IActionResult CurrencyUnit()
+        {
+            return View();
+        }
+        public IActionResult FixedAssetManagement()
+        {
+            return View();
+        } 
+        public IActionResult Budgeting()
+        {
+            return View();
+        }
+        public IActionResult Capitations()
+        {
+            return View();
+        } 
+        public IActionResult OpeningBalances()
+        {
+            return View();
+        }
+    }
+}
